9d6903eb8bab52cdadd1ad517fb222fcccd369c2
nucklehead <pierevans@gmail.com>
2017-10-10 02:29:48 +0000

    Working on controllers

--- src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/AccountController.java
+++ src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/AccountController.java
@@ -1,15 +1,22 @@
 package com.aigleinfo.siksepounoutout.rest.controllers;
 
 import io.swagger.annotations.Api;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aigleinfo.siksepounoutout.repository.AccountRepository;
 
 @RestController
 @RequestMapping("/api/account")
 @Api
 public class AccountController {
+    @Autowired
+    AccountRepository accountRepository;
+  
   @RequestMapping("/")
-  public String index() {
+  public String get() {
     return "Greetings from Spring Boot!";
   }
 }
--- src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/CommentController.java
+++ src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/CommentController.java
@@ -1,13 +1,19 @@
 package com.aigleinfo.siksepounoutout.rest.controllers;
 
 import io.swagger.annotations.Api;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aigleinfo.siksepounoutout.repository.CommentRepository;
 
 @RestController
 @RequestMapping("/api/comment")
 @Api
 public class CommentController {
+    @Autowired
+    CommentRepository commentRepository;
   @RequestMapping("/")
   public String index() {
     return "Greetings from Spring Boot!";
