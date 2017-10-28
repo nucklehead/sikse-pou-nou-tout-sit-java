f7f3b36f7808914467cefd8c4491212229ac56d7
nucklehead <pierevans@gmail.com>
2017-10-28 05:26:18 +0000

    Broke out thymeleaf templates
    added webjars instead of bower js
    
    added restb for facebook
    
    added bootstrap template

--- src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/VideoController.java
+++ src/main/java/com/aigleinfo/siksepounoutout/rest/controllers/VideoController.java
@@ -2,9 +2,16 @@ package com.aigleinfo.siksepounoutout.rest.controllers;
 
 import com.aigleinfo.siksepounoutout.model.Video;
 import com.aigleinfo.siksepounoutout.repository.VideoRepository;
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.Version;
+import com.restfb.types.VideoList;
 import io.swagger.annotations.Api;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +19,9 @@ import org.springframework.web.bind.annotation.*;
 @Api
 public class VideoController {
   @Autowired VideoRepository videoRepository;
+
+  @Value("${facebook.token}")
+  String facebookToken;
 
   @RequestMapping(method = RequestMethod.GET, path = "")
   public List<Video> getVideos() {
@@ -37,5 +47,14 @@ public class VideoController {
   @RequestMapping(method = RequestMethod.DELETE, path = "/{videoID}")
   public void deleteVideoById(@PathVariable String videoID) {
     videoRepository.delete(videoID);
+  }
+
+  @RequestMapping(method = RequestMethod.GET, path = "/facebook")
+  public List<VideoList> getFacebookVideos() {
+    FacebookClient facebookClient = new DefaultFacebookClient(facebookToken, Version.VERSION_2_9);
+    Connection<VideoList> fbVideos =
+        facebookClient.fetchConnection(
+            "Aigleinfo/videos", VideoList.class, Parameter.with("limit", 100));
+    return fbVideos.getData();
   }
 }
--- src/main/java/com/aigleinfo/siksepounoutout/web/AppController.java
+++ src/main/java/com/aigleinfo/siksepounoutout/web/AppController.java
@@ -1,6 +1,9 @@
 package com.aigleinfo.siksepounoutout.web;
 
+import com.aigleinfo.siksepounoutout.rest.controllers.OptionController;
+import com.aigleinfo.siksepounoutout.rest.controllers.VideoController;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Api
 public class AppController {
 
+  @Autowired OptionController optionController;
+
+  @Autowired VideoController videoController;
+
   @RequestMapping(method = RequestMethod.GET, path = "/")
   public String index(Model model) {
     model.addAttribute("name", "JP");
     model.addAttribute("title", "Home Page");
+    model.addAttribute("optionTitle", "Opsyon ki genyen yo:");
+
+    model.addAttribute("options", optionController.getOptions());
+    model.addAttribute("fbVideos", videoController.getFacebookVideos());
+    model.addAttribute("pageVideoPath", "Aigleinfo/videos");
 
     return "home";
   }
